0f8635892abd52a943eae34a78cf4c6f0dbf0d1f
Orhan ARIKAN <arikanorh@gmail.com>
2016-03-22 19:49:46 +0200

    Implements googkle-ouaht2 login
    closes #1

--- src/com/vebora/oauth2/servlet/WelcomeServlet.java
+++ src/com/vebora/oauth2/servlet/WelcomeServlet.java
@@ -1,7 +1,6 @@
 package com.vebora.oauth2.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,9 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.services.gmail.model.Thread;
 import com.google.api.services.oauth2.model.Userinfoplus;
-import com.vebora.oauth2.cache.services.GmailService;
 import com.vebora.oauth2.cache.services.GoogleUserInfoService;
 import com.vebora.oauth2.main.user.UserIdSupplier;
 
@@ -39,25 +36,6 @@ public class WelcomeServlet extends HttpServlet
 		resp.getWriter().println("<img src='" + info.getPicture() + "' width=50px height:50px' />");
 		resp.getWriter().println("Welcome :" + info.getName().toUpperCase());
 		resp.getWriter().println("<br>");
-
-		// YoutubeService.getSomething(userId);
-
-		List<Thread> threads = GmailService.getThreads(userId);
-
-		for (Thread thread : threads)
-		{
-			resp.getWriter().println(" - " + thread.toPrettyString() + "<br>");
-		}
-
-		// try
-		// {
-		// GmailService.getMessages(userId);
-		//
-		// } catch (MessagingException e)
-		// {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
 
 	}
 }
